c1b0088341a8e69a7eebf2577f3f1a95013b2221
dark0fHorizon <mishaal077@gmail.com>
2020-12-16 00:19:42 +0530

    Added iframeView 15 - 12 - 2020

--- Site_Source_Codes/assets/js/main.js
+++ Site_Source_Codes/assets/js/main.js
@@ -1,3 +1,170 @@
+<<<<<<< HEAD
+
+//Content Link Variables
+
+if ($('#btn-content').val() == "display_current_date_time") {
+  
+}
+var display_current_date_time = '<iframe src="Display-Current-Time-and-Date.html" class="content-frame" frameborder=""></iframe>';
+
+//End
+
+// main left div list Search Filter Script Start
+function mysearchlistFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("src-inp");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("search-box-list");
+  li = ul.getElementsByClassName("src-list");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByClassName("src-anc")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+// main left div list Search Filter Script end
+
+// login box show hide start
+$(".navigation-bar-login-div-Toggle").click(function () {
+  $('#navigation-bar-login-div-function').slideToggle();
+});
+// login box show hide End
+
+// Search box show hide start
+$(".unervisal-search-button-Toggle").click(function () {
+  $('#navigation-bar-search-div-function').slideToggle();
+});
+// Search box show hide end
+
+// Full Search Filter Script Start
+$("#uni-btn").click(function () {
+  var filter = $('#uni-src-inp').val(),
+    count = 0;
+  $('.video-main-div').each(function () {
+    if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+      $(this).fadeOut();
+    } else {
+      $(this).fadeIn('slow');
+      count++;
+    }
+  });
+  var resultVal = $("#res-val").val();
+  document.getElementById("res-val").innerHTML = resultVal;
+
+  if ($(window).width() <= 600) {
+    $('.navigation-bar-search').fadeOut();
+    $('.navigation-bar-login-div').fadeOut();
+  }
+
+  $('.clear-search-div').fadeIn();
+  $('.search-one-result').fadeIn();
+});
+
+$('#uni-src-inp').keypress(function (event) {
+  var keycode = (event.keyCode ? event.keyCode : event.which);
+  if (keycode == '13') {
+    var filter = $('#uni-src-inp').val(),
+      count = 0;
+    $('.video-main-div').each(function () {
+      if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+        $(this).fadeOut();
+      } else {
+        $(this).fadeIn('slow');
+        count++;
+      }
+    });
+    var resultVal = $("#result-val").val();
+    $("result-val").text(resultVal);
+
+    if ($(window).width() <= 600) {
+      $('.navigation-bar-search').fadeOut();
+      $('.navigation-bar-login-div').fadeOut();
+    }
+    $('.clear-search-div').fadeIn();
+    $('.search-one-result').fadeIn();
+  }
+});
+// Full Search Filter Script End
+
+// search box show hide start
+function searchboxshowhidefunction() {
+  var x = document.getElementById("navigation-bar-search-div-function");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+// search box show hide start
+
+//clear search
+// var blanktxt = $.trim( $('.unervisal-search-input').val() )
+var blanktxt = "";
+$('.clear-search-div').click(function () {
+  $('.unervisal-search-input').val(blanktxt);
+  $('.clear-search-div').fadeOut();
+  $('.search-one-result').fadeOut();
+  $('.video-main-div').fadeIn();
+});
+
+//Button pointer click
+$(function () {
+
+  //creating a style object for the ripple effect
+  function RippleStyle(width, height, posX, posY) {
+    this.width = (width <= height) ? height : width;
+    this.height = (width <= height) ? height : width;
+    this.top = posY - (this.height * 0.5);
+    this.left = posX - (this.width * 0.5);
+  }
+
+  $('.button-animation').on('mousedown', function (e) {
+    //appending an element with a class name "btn-ripple"
+    var rippleEl = $('<span class="btn-ripple"></span>').appendTo(this);
+
+    //getting the button's offset position
+    var pos = $(this).offset();
+
+    //get the button's width and height
+    var width = $(this).outerWidth();
+    var height = $(this).outerHeight();
+
+    //get the cursor's x and y position within the button
+    var posX = e.pageX - pos.left;
+    var posY = e.pageY - pos.top;
+
+    //adding a css style to the ripple effect
+    var rippleStyle = new RippleStyle(width, height, posX, posY);
+    rippleEl.css(rippleStyle);
+  });
+
+  //this event listener will be triggered once the ripple animation is done
+  $('.button-animation').on('animationend webkitAnimationEnd oanimationend MSAnimationEnd', '.button-animation-ripple', function () {
+    $(this).remove();
+  });
+});
+
+
+function iframeView(link) {
+  $('.right-main-div').append(link);
+}
+
+function delCurrentView() {
+  $('.temp_div').remove();
+  event.preventDefault();
+}
+
+$('.video-main-button').click(function () {
+  delCurrentView();
+  //iframeView($(this).val());
+  alert($(this).val().con);
+});
+
+=======
   // main left div list Search Filter Script Start
   function mysearchlistFunction() {
     var input, filter, ul, li, a, i, txtValue;
@@ -143,3 +310,4 @@
        $(this).remove();
      });
    });
+>>>>>>> 58e1c2fb9b5983d8f49e21e23535010e4be3db4c
