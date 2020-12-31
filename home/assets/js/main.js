752e27b629cf97a7ff3f15b50b91bd1183dd3a70
dark0fHorizon <mishaal077@gmail.com>
2020-12-31 21:03:43 +0530

    Added Preloader

--- home/assets/js/main.js
+++ home/assets/js/main.js
@@ -1,19 +1,32 @@
+// ================= Preloader ===================
+
+// $(document).load(function() {
+// 	$(".loader").delay(2000).hide("slow");
+//   $("#overlayer").delay(2000).hide("slow");
+// })
+window.onload = function () {
+    //hide the preloader
+    $(".loader").delay(2000).fadeOut("fast");
+    $("#overlayer").delay(2000).fadeOut("slow");
+}
+
+
 if (localStorage.getItem('selected-theme') === 'dark') {
     document.getElementById('theme-button').checked = true;
     $('#theme-ico').removeClass('bx bxs-sun');
     $('#theme-ico').addClass('bx bxs-moon');
 
 } else {
-  document.getElementById('theme-button').checked = false;
-  $('#theme-ico').removeClass('bx bxs-moon');
-  $('#theme-ico').addClass('bx bxs-sun');
+    document.getElementById('theme-button').checked = false;
+    $('#theme-ico').removeClass('bx bxs-moon');
+    $('#theme-ico').addClass('bx bxs-sun');
 
 }
 if (localStorage.getItem('selected-theme') === 'dark') {
     document.getElementById('theme-button2').checked = true;
 
 } else {
-  document.getElementById('theme-button2').checked = false;
+    document.getElementById('theme-button2').checked = false;
 
 }
 
@@ -21,30 +34,30 @@ if (localStorage.getItem('selected-theme') === 'dark') {
 
 
 /*==================== SHOW MENU ====================*/
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
-    
+        nav = document.getElementById(navId)
+
     // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             // We add the show-menu class to the div tag with the nav__menu class
             nav.classList.toggle('show-menu')
         })
     }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
-  $('.nav-profile-button').click(function () { 
-      $('.profile-content').fadeToggle();      
-  });
+$('.nav-profile-button').click(function () {
+    $('.profile-content').fadeToggle();
+});
 
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
@@ -54,40 +67,40 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
 window.addEventListener('scroll', scrollActive)
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
-function scrollHeader(){
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
     const nav = document.getElementById('header')
     // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    if (this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*==================== SHOW SCROLL TOP ====================*/ 
-function scrollTop(){
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
     const scrollTop = document.getElementById('scroll-top');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+    if (this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
 const themeButton = document.getElementById('theme-button')
 const mthemeButton = document.getElementById('theme-button2')
 
@@ -105,10 +118,10 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moo
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
- 
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+
 }
 
 // Activate / deactivate the theme manually with the button
@@ -120,11 +133,11 @@ themeButton.addEventListener('click', () => {
     if ($('#theme-ico').hasClass('bx bxs-sun')) {
         $('#theme-ico').removeClass('bx bxs-sun');
         $('#theme-ico').addClass('bx bxs-moon');
-    } else if($('#theme-ico').hasClass('bx bxs-moon')){
+    } else if ($('#theme-ico').hasClass('bx bxs-moon')) {
         $('#theme-ico').removeClass('bxs-moon');
         $('#theme-ico').addClass('bx bxs-sun');
     }
-      
+
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
@@ -134,7 +147,7 @@ mthemeButton.addEventListener('click', () => {
     // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-      
+
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
